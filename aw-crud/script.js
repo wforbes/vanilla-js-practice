376b8d628f99f24478e13c7c491131001e52f40b
Will Forbes <wforbes87@gmail.com>
2024-12-05 00:33:32 -0800

    aw-crud: add sessionStorage for list items

--- aw-crud/script.js
+++ aw-crud/script.js
@@ -1,20 +1,53 @@
 let inputText = document.querySelector("#add-txt");
 let inputButton = document.querySelector("#add-btn");
+let itemList = document.querySelector("#item-list");
+
+let listItems = [];
+let sessionStorageKey = "listItems";
+let savedListItems = sessionStorage.getItem(sessionStorageKey);
+
+init();
+
+function init() {
+	inputButton.addEventListener("click", handleClick);
+	setupSessionStorage();
+}
+
+function setupSessionStorage() {
+	if (savedListItems) {
+		listItems = JSON.parse(savedListItems);
+		renderListItems();
+	} else {
+		sessionStorage.setItem(sessionStorageKey, JSON.stringify(listItems));
+	}
+}
+
+function renderListItems() {
+	itemList.innerHTML = "";
+	listItems.forEach((item) => {
+		let newListItem = document.createElement("li");
+		newListItem.classList.add("list-item");
+		newListItem.textContent = item;
+		itemList.appendChild(newListItem);
+	});
+}
+
+function saveListItems() {
+	sessionStorage.setItem(sessionStorageKey, JSON.stringify(listItems));
+}
 
 function handleClick() {
 	let listItemText = inputText.value;
 	if (!listItemText || listItemText === "") return;
-
-	let list = document.querySelector("#item-list");
 
 	let newListItem = document.createElement("li");
 	newListItem.classList.add("list-item");
 	let newContent = document.createTextNode(listItemText);
 
 	newListItem.appendChild(newContent);
-	list.appendChild(newListItem);
+	itemList.appendChild(newListItem);
 
 	inputText.value = "";
+	listItems.push(listItemText);
+	saveListItems();
 }
-
-inputButton.addEventListener("click", handleClick)
